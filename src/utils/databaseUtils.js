bb25f3b4697b74a0159cbd652c2373b81d68be1c
Anthony Samuel <masamuel@zuelligpharma.com>
2025-05-06 16:29:16 +0700

    asfasf

--- src/utils/databaseUtils.js
+++ src/utils/databaseUtils.js
@@ -3,7 +3,7 @@ import { getApiUrl } from '../config/api';
 
 export const fetchAllSnapshots = async () => {
   try {
-    const response = await axiosInstance.get('/api/snapshots');
+    const response = await axiosInstance.get('/snapshots');
     if (!response.data) {
       throw new Error('No data received from server');
     }
